526f6650ab7a404130ce015ce10baf9b56040718
pranav-027 <pd2772001@gmail.com>
2026-06-09 16:06:54 +0530

    unstaged stuff

---
+++ homework/cakebaker/src/main/java/com/homework/cakebaker/CakebakerApplication.java
@@ -0,0 +1,14 @@
+package com.homework.cakebaker;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakebakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakebakerApplication.class, args);
+	}
+
+}
---
+++ homework/cakebaker/src/test/java/com/homework/cakebaker/CakebakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.cakebaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakebakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
